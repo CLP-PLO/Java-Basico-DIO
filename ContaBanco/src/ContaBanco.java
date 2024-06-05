a7dda4dd7c2948ee9963e05cf0df6fc181a2e752
PauloF <plofer.rs@gmail.com>
2024-06-05 17:39:26 -0300

    meu projeto simulação conta de banco

---
+++ ContaBanco/src/ContaBanco.java
@@ -0,0 +1,15 @@
+public class App {
+
+    int final AGENCIA = 0123;
+    String final CONTA = "9876-5";
+    boolean tipoContaCorrente = true;
+    String nomeCliente = "Paulo";
+    short saldo = 6543.21;
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
